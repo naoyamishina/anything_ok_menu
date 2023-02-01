af950899c66ad4c6ac9a84c9c28bcc505181b96e
mishina naoya <msnyxx811@gmail.com>
2023-02-01 19:39:43 +0900

    fix: comment機能のバリデーション対応

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,10 @@
 class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.build(comment_params)
-    @comment.save
+    if @comment.save
+    else
+      redirect_to menu_path(@comment.menu), danger: t('defaults.message.not_created', item: Comment.model_name.human)
+    end
   end
 
   def destroy
