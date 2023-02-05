054ee25664de1f087fb7d9b271556f35edfbc33b
mishina naoya <msnyxx811@gmail.com>
2023-02-06 06:48:37 +0900

    fix: sample.jpegをmenu_placeholder.pngに

--- app/uploaders/menu_image_uploader.rb
+++ app/uploaders/menu_image_uploader.rb
@@ -13,9 +13,9 @@ class MenuImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  #画像がアップロードされてない時に、'sample.jpg'を表示する。この画像はassets/images配下に事前に配置しておく。
+  #画像がアップロードされてない時に、'menu_placeholder.png'を表示する。この画像はassets/images配下に事前に配置しておく。
   def default_url
-    'sample.jpeg'
+    'menu_placeholder.png'
   end
   
   #アップロードを許可するファイル種類を指定する。
