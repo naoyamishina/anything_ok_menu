ed17092ef2ff12c12412426bd324cd83df799c8c
mishina naoya <msnyxx811@gmail.com>
2023-02-07 19:49:53 +0900

    fix: ユーザー登録後メニュー一覧へ遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path, success: t('.success')
+      auto_login(@user)
+      redirect_to menus_path, success: t('.success')
     else
       flash.now[:danger] = t('.fail')
       render :new, status: :unprocessable_entity
