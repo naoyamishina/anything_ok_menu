d5d99e8811f0134285f8c1989ca473a39a24253f
mishina naoya <msnyxx811@gmail.com>
2023-02-09 21:16:36 +0900

    rails g config:install

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,9 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.reset_password_email.subject
   #
-  def reset_password_email
+  def reset_password_email(user)
     @user = User.find(user.id)
     @url = edit_password_reset_url(@user.reset_password_token)
-    mail(to: user.email, subject: 'パスワードリセット')
+    mail(to: user.email, subject: t('defaults.password_reset'))
   end
 end
