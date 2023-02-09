8e83a8475e46ddd9cea7f11f06166649d1ab4a66
mishina naoya <msnyxx811@gmail.com>
2023-02-09 18:13:59 +0900

    rails g sorcery:install reset_password --only-submodules

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_124903) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_09_091121) do
   create_table "comments", charset: "utf8mb4", force: :cascade do |t|
     t.text "body", null: false
     t.bigint "user_id", null: false
@@ -51,7 +51,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_124903) do
     t.datetime "updated_at", null: false
     t.string "avatar"
     t.integer "gender", default: 0, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
   add_foreign_key "comments", "menus"
