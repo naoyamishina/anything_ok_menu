b1569c8ebe85748540a56be005029dc1b830943c
mishina naoya <msnyxx811@gmail.com>
2023-02-11 10:06:42 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,11 @@ Rails.application.routes.draw do
   resources :likes, only: %i[create destroy]
   resource :profile, only: %i[edit update]
   resources :password_resets, only: %i[new create edit update]
+
+  namespace :admin do
+    root to: 'dashboards#index'
+    get 'login', to: 'user_sessions#new'
+    post 'login', to: 'user_sessions#create'
+    delete 'logout', to: 'user_sessions#destroy'
+  end
 end
