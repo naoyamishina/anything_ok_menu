d616ee3baa6cba2a3a2e807934190de03ca70194
mishina naoya <msnyxx811@gmail.com>
2023-02-11 13:20:44 +0900

    ユーザ/メニューのCRUD機能

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,7 @@ Rails.application.routes.draw do
     get 'login', to: 'user_sessions#new'
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
+    resources :menus, only: %i[index edit update show destroy]
+    resources :users, only: %i[index edit update show destroy]
   end
 end
