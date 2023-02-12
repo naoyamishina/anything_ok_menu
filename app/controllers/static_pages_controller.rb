de8c6a95b9e9e4fc067ff2beb9172ffa2d031207
mishina naoya <msnyxx811@gmail.com>
2023-02-12 11:38:04 +0900

    HTML雛形作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: %i[top]
+  skip_before_action :require_login
   
-  def top
-  end
+  def top; end
+  def privacy; end
+  def terms; end
 end
