6d2edd3db3af28f7ef07e6ce1c15011c5ac012f0
mishina naoya <msnyxx811@gmail.com>
2023-02-26 13:53:28 +0900

    gamilの連携

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,9 +65,23 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { :host => 'https://anything-ok-menu.herokuapp.com/' }
+  config.action_mailer.default_url_options = { protocol: 'https', host:'https://anything-ok-menu.herokuapp.com'}
+#ホストの設定
+
+  config.action_mailer.delivery_method = :smtp
+  #smtpを使う
+
+  config.action_mailer.smtp_settings = {
+      port: 587,
+      address:"smtp.gmail.com",
+      domain: 'gmail.com', #Gmailを使う場合
+      user_name: ENV['GMAIL_ADDRESS'], #Gmailアカウントのメールアドレス
+      password: ENV['GMAIL_PASSWORD'], #Gmailで設定したアプリパスワード
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
