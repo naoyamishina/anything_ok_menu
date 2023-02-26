2c536d89fa701ea84d02f1a51565b804bb412a64
mishina naoya <msnyxx811@gmail.com>
2023-02-26 12:52:34 +0900

    本番環境のメール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,15 +67,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    domain: 'gmail.com',
-    port: 587,
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
-
+  config.action_mailer.default_url_options = { :host => 'https://anything-ok-menu.herokuapp.com/' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
