b88f3af419148b2fd4f8ddae39057a8d53e8791f
naoyamishina <102893104+naoyamishina@users.noreply.github.com>
2023-02-26 11:23:22 +0900

    Revert "Deploy"

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,6 @@ CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_directory  = 'anythingokmenu'
-  config.asset_host = 'https://s3.amazonaws.com/anythingokmenu'
-  config.fog_public = false 
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
