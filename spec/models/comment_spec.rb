476ed6532a832415e26132bba6bbe829aae30c97
mishina naoya <msnyxx811@gmail.com>
2023-03-07 21:30:07 +0900

    rspec/model/commentの修正

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,31 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  describe "コメント作成のバリデーションテスト" do
 
-  context '全てのフィールドが有効な場合' do
-    it '有効であること' do
+    it 'すべてのカラムにデータがあれば有効であること' do
       comment = build(:comment)
       expect(comment).to be_valid
     end
-  end
 
-  context '本文が存在しない場合' do
-    it '無効であること' do
+    it 'コメント本文がなければ無効であること' do
       comment = build(:comment, body: nil)
       expect(comment).to be_invalid
       expect(comment.errors[:body]).to include('を入力してください')
     end
-  end
 
-  context '本文が65535文字以内の場合' do
-    it '有効であること' do
+    it 'コメント本文が65535文字以下であれば有効であること' do
       comment = build(:comment, body: 'a' * 65535)
       expect(comment).to be_valid
     end
-  end
 
-  context '本文が65536文字以上の場合' do
-    it '無効であること' do
+    it 'コメント本文が65536文字以上であれば有効であること' do
       comment = build(:comment, body: 'a' * 65536)
       expect(comment).to be_invalid
       expect(comment.errors[:body]).to include('は65535文字以内で入力してください')
